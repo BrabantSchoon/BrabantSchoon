7db5bcb57f378754127e8016e89fa9dd8308ec33
BrabantSchoon <info@brabantschoon.nl>
2026-07-27 00:31:32 +0200

    Add files via upload

--- generate.py
+++ generate.py
@@ -76,7 +76,7 @@ EMAIL = "info@brabantschoon.nl"
 WA_LINK = "https://wa.me/31492313050?text=Hoi%2C%20ik%20wil%20graag%20een%20offerte%20aanvragen"
 KVK = "99274175"
 CITY = "Helmond"
-ASSET_VERSION = "112"
+ASSET_VERSION = "113"
 
 # ---------------------------------------------------------------
 # ICONS
